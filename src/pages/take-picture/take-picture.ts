a055e4fa3666dad12e50abad6d56d52a5dd12433
Douglas Batista Santos <dbatists@everis.com>
2019-03-08 13:27:42 -0300

    versão mais estável

--- src/pages/take-picture/take-picture.ts
+++ src/pages/take-picture/take-picture.ts
@@ -32,7 +32,6 @@ export class TakePicturePage {
       quality: 100,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
-      allowEdit: true,
       mediaType: this.camera.MediaType.PICTURE,
       targetHeight: 600,
       targetWidth: 900
